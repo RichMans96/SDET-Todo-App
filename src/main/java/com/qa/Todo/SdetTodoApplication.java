dab6d71104ebd7ab4489428c5cdf0dc27ed4e193
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-03-15 09:46:44 +0000

    first commit

---
+++ src/main/java/com/qa/Todo/SdetTodoApplication.java
@@ -0,0 +1,13 @@
+package com.qa.Todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdetTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdetTodoApplication.class, args);
+	}
+
+}
