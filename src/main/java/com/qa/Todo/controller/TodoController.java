fcf1a3017343fdeb0daffa63730f078b2589c789
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-03-16 14:53:04 +0000

    TODO-6 #comment added read by id and added an exception for todo not found

--- src/main/java/com/qa/Todo/controller/TodoController.java
+++ src/main/java/com/qa/Todo/controller/TodoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,13 @@ public class TodoController {
 		List<TodoDTO> todos = todoService.readAllTodos();
 		
 		return new ResponseEntity<List<TodoDTO>>(todos, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<TodoDTO> todoById(@PathVariable("id") Integer id) {
+		TodoDTO todo = todoService.todoById(id);
+		
+		return new ResponseEntity<TodoDTO>(todo, HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/qa/Todo/service/TodoService.java
+++ src/main/java/com/qa/Todo/service/TodoService.java
@@ -2,6 +2,7 @@ package com.qa.Todo.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.qa.Todo.data.model.Todo;
 import com.qa.Todo.data.repository.TodoRepository;
 import com.qa.Todo.dto.TodoDTO;
+import com.qa.Todo.exceptions.TodoNotFoundException;
 import com.qa.Todo.mappers.TodoMapper;
 
 @Service
@@ -37,5 +39,15 @@ public class TodoService {
 		todos.forEach(todo -> todoDTOs.add(todoMapper.mapToDTO(todo)));
 		
 		return todoDTOs;
+	}
+	
+	public TodoDTO todoById(Integer id) {
+		Optional<Todo> todo = todoRepository.findById(id);
+		
+		if(todo.isPresent()) {
+			return todoMapper.mapToDTO(todo.get());
+		} else {
+			throw new TodoNotFoundException("Todo doesn't exist!");
+		}
 	}
 }
