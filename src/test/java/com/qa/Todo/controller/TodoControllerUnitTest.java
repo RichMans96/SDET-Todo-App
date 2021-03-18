184a7a59d08bf46d16af1228f3ba86f7c7576c3b
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-03-18 13:42:30 +0000

    TODO-22 #comment read tests written and pass for controller

--- src/test/java/com/qa/Todo/controller/TodoControllerUnitTest.java
+++ src/test/java/com/qa/Todo/controller/TodoControllerUnitTest.java
@@ -1,5 +1,72 @@
 package com.qa.Todo.controller;
 
-public class TodoControllerUnitTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.qa.Todo.data.model.Todo;
+import com.qa.Todo.data.model.TodoList;
+import com.qa.Todo.dto.TodoDTO;
+import com.qa.Todo.service.TodoService;
+
+@SpringBootTest
+public class TodoControllerUnitTest {
+	
+	@Autowired
+	private TodoController todoController;
+	
+	@MockBean
+	private TodoService todoService;
+	
+	private List<Todo> todos;
+	private List<TodoDTO> todoDTOs;
+	
+	private Todo todo;
+	private TodoDTO todoDTO;
+	private TodoList todoList;
+
+	@BeforeEach
+	public void init() {
+		todoList = new TodoList(1, "Morning");
+		todo = new Todo(1, "Brush teeth", true, todoList);
+		todoDTO = new TodoDTO(1, "Brush teeth", true);
+		
+		todos = new ArrayList<Todo>();
+		todoDTOs = new ArrayList<TodoDTO>();
+	}
+	
+	@Test
+	public void getAllTodosTest() {
+		when(todoService.readAllTodos()).thenReturn(todoDTOs);
+		
+		ResponseEntity<List<TodoDTO>> response = new ResponseEntity<List<TodoDTO>>(todoDTOs, HttpStatus.OK);
+		
+		assertThat(response).isEqualTo(todoController.getAllTodos());
+		
+		verify(todoService, times(1)).readAllTodos();
+	}
+	
+	@Test
+	public void getTodoByIdTest() {
+		when(todoService.todoById(Mockito.any(Integer.class))).thenReturn(todoDTO);
+		
+		ResponseEntity<TodoDTO> response = new ResponseEntity<TodoDTO>(todoDTO, HttpStatus.OK);
+	
+		assertThat(response).isEqualTo(todoController.todoById(todo.getId()));
+		
+		verify(todoService, times(1)).todoById(Mockito.any(Integer.class));
+	}
 }
