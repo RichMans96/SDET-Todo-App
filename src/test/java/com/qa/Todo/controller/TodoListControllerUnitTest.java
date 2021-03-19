a9a7df21e4bb9f971836660e73ee45ed9a1c8554
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-03-19 13:11:34 +0000

    TODO-32 #comment created read all test for todolist controller unit test

--- src/test/java/com/qa/Todo/controller/TodoListControllerUnitTest.java
+++ src/test/java/com/qa/Todo/controller/TodoListControllerUnitTest.java
@@ -1,5 +1,67 @@
 package com.qa.Todo.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.qa.Todo.data.model.TodoList;
+import com.qa.Todo.dto.TodoDTO;
+import com.qa.Todo.dto.TodoListDTO;
+import com.qa.Todo.service.TodoListService;
+
+@SpringBootTest
 public class TodoListControllerUnitTest {
 
+	@Autowired
+	private TodoListController todoListController;
+	
+	@MockBean
+	private TodoListService todoListService;
+	
+	private List<TodoList> todoLists;
+	private List<TodoListDTO> todoListDTOs;
+	private List<TodoDTO> todos;
+	
+	private TodoDTO todo;
+	private TodoList todoList;
+	private TodoListDTO todoListDTO;
+	
+	@BeforeEach
+	public void init() {
+		todo = new TodoDTO(1, "brush teeth", false);
+		todoList = new TodoList(1, "Morning");
+		todoListDTO = new TodoListDTO(1, "Morning", todos);
+		
+		todos = new ArrayList<TodoDTO>();
+		todoLists = new ArrayList<TodoList>();
+		todoListDTOs = new ArrayList<TodoListDTO>();
+		
+		todos.add(todo);
+		todoLists.add(todoList);
+		todoListDTOs.add(todoListDTO);
+	}
+	
+	@Test
+	public void getAllTodoLists() {
+		when(todoListService.readAllTodoLists()).thenReturn(todoListDTOs);
+		
+		ResponseEntity<List<TodoListDTO>> response = new ResponseEntity<List<TodoListDTO>>(todoListDTOs, HttpStatus.OK);
+		
+		assertThat(response).isEqualTo(todoListController.getAllTodoLists());
+		
+		verify(todoListService, times(1)).readAllTodoLists();
+	}
+	
 }
