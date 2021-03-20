3e1bc171920586f7e3b5abaad03d140e1d6fc8d4
RichMans96 <richard.mansworth@hotmail.co.uk>
2021-03-20 17:21:32 +0000

    TODO-35 #comment can now create a list, design needs to be neatened up but functionality is there

---
+++ src/main/frontend/js/createList.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('#createList');
+
+form.addEventListener('submit', (event) => {
+  createList(event);
+});
+
+function createList(event) {
+  event.preventDefault();
+
+  const listName = document.querySelector('#listname').value;
+
+  fetch('http://localhost:8080/todolist', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      listName: listName,
+    }),
+  });
+
+  alert('Created successfully!');
+}
